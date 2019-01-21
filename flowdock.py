5073f0c5ba38a3605618ea456c1e15229e4e30ba
Apua <Apua.A.Aa@gmail.com>
2019-01-21 08:18:24 +0800

    Implement sending message

--- flowdock.py
+++ flowdock.py
@@ -1,9 +1,49 @@
+import requests
+
+API = 'https://api.flowdock.com'
+STREAM = 'https://stream.flowdock.com'
+
+
+def get_uid(auth, user, org=None):
+    if isinstance(user, int):
+        uid = user
+    elif user in get_uid.users:
+        uid = get_uid.users[user]
+    else:
+        resp = requests.get(f'{API}/users' if org is None else f'{API}/organizations/{org}/users', auth=auth)
+        assert resp.status_code == 200, (resp.status_code, resp.json())
+        get_uid.users.update({u['nick']: u['id'] for u in resp.json()})
+        uid = get_uid.users[user]
+
+    return uid
+get_uid.users = {}
+
+
 def flow(token, org, flow):
     r"""
     -   not support emoji
     -   support stream API
+
+    >>> from tests.tokens import PERSONAL_API_TOKEN
+    >>> myflow = flow(PERSONAL_API_TOKEN, 'hpe', 'apua-flow')
+    >>> msg_id = myflow.send('doctest')
+    >>> myflow.show(msg_id)
     """
+    auth = (token, '')
+
+    def send(content, tags=None):
+        json = {'event': 'message', 'content': content, 'tags': tags}
+        resp = requests.post(f'{API}/flows/{org}/{flow}/messages', auth=auth, json=json)
+        assert resp.status_code == 201, (resp.status_code, resp.json())
+        return resp.json()['id']
+
+    def show(msg_id):
+        resp = requests.get(f'{API}/flows/{org}/{flow}/messages/{msg_id}', auth=auth)
+        assert resp.status_code == 200, (resp.status_code, resp.json())
+        return resp.json()
+
     obj = lambda: None
+    obj.__dict__.update(locals())
     return obj
 
 
@@ -11,8 +51,27 @@ def private_message(token, user, org=None):
     r"""
     -   cache user ID at module-level
     -   use `{org}` if provided
+
+    >>> from tests.tokens import PERSONAL_API_TOKEN
+    >>> ray = private_message(PERSONAL_API_TOKEN, 'Ray_')
+    >>> msg_id = ray.send('doctest')
+    >>> ray.show(msg_id)
     """
+    auth = (token, '')
+    uid = get_uid(auth, user, org)
+
+    def send(content):
+        resp = requests.post(f'{API}/private/{uid}/messages', auth=auth, json={'event':'message','content':content})
+        assert resp.status_code == 201, (resp.status_code, resp.json())
+        return resp.json()['id']
+
+    def show(msg_id):
+        resp = requests.get(f'{API}/private/{uid}/messages/{msg_id}', auth=auth)
+        assert resp.status_code == 200, (resp.status_code, resp.json())
+        return resp.json()
+
     obj = lambda: None
+    obj.__dict__.update(locals())
     return obj
 
 
