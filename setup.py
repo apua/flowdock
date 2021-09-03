16f3ad419b4b1fcac23cb29c3f5841cd79ad5d54
Apua <Apua.A.Aa@gmail.com>
2021-09-03 21:03:32 +0800

    Add Python3.x supports

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ setuptools.setup(
         'Intended Audience :: System Administrators',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Communications :: Chat',
         'Topic :: Communications :: Conferencing',
